8f782efba7bf6e9b114e19da48945a5b2a944114
Faizulla <fant@zzz.xyz>
2021-10-03 22:41:04 +0500

    Front is done

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main () => runApp(
@@ -8,17 +9,13 @@ void main () => runApp(
 );
 
 class MyApp extends StatefulWidget {
-  @overrride
-  State<StatefulWidget> createState () {
-    return _MyApp();
-  }
+  @override
+  State<StatefulWidget> createState () => _MyApp();
 }
 
 class _MyApp extends State<MyApp>{
   @override
-  Widget build(BuildContext context) {
-    // TODO: implement build
-    return Scaffold(
+  Widget build(BuildContext context) => Scaffold(
       body: Column(
         children: [
           Container(
@@ -26,7 +23,8 @@ class _MyApp extends State<MyApp>{
             width: MediaQuery.of(context).size.width,
             color: Color(0xfff1f1f1),
             child: Column(
-              children: [
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: const [
                 Text(
                   "Location",
                   style: TextStyle(
@@ -35,13 +33,61 @@ class _MyApp extends State<MyApp>{
                 ),
                 ),
                 Padding(
-                  padding: ,
+                  padding: EdgeInsets.only(top: 10.0, bottom: 10.0),
+                  child: Text(
+                    "Temperature",
+                    style: TextStyle(
+                      color: Colors.purple,
+                      fontSize: 40.0,
+                      fontWeight: FontWeight.w900
+                    ),
+                  ),
+                ),
+                Text(
+                  "High of temp, Low of temp",
+                  style: TextStyle(
+                    color: Color(0xff9e9e9e),
+                    fontSize: 14.0,
+                    fontWeight: FontWeight.w600,
+                  ),
                 )
               ],
             ),
-          )
+          ),
+          Expanded(
+            child: Padding(
+              padding: EdgeInsets.all(20.0),
+                child: ListView(
+                  children: [
+                    ListTile(
+                      leading: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Icon(Icons.thermostat_outlined, color: Colors.blueAccent,)
+                        ],
+                      ),
+                      title: Text(
+                        "Tmperature",
+                        style: TextStyle(
+                          fontSize: 18.0,
+                          fontWeight: FontWeight.w600
+                        ),
+                      ),
+                      subtitle: Text(
+                        "SubTitle",
+                        style: TextStyle(
+                          fontSize: 16.0,
+                          fontWeight: FontWeight.w600,
+                          color: Color(0xff9e9e9e)
+                        ),
+                      ),
+                    )
+                  ],
+                )
+              ),
+            ),
+
         ],
       ),
     );
-  }
 }
---
+++ lib/widgets/weather_tile.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+class WeatherTile extends StatelessWidget{
+
+}
