c1d74c81139f74c7a8b325516deb9d9ebf4d178c
Faizulla <fant@zzz.xyz>
2021-10-06 20:19:21 +0500

    Api done

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,54 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'widgets/weather_tile.dart';
+import 'package:http/http.dart' as http;
+import 'dart:async';
+import 'dart:convert';
+
+Future<WeatherInfo> fetcWeather() async{
+  final zipCode = "125464" ;
+  final apiKey = ;
+  final requestUrl = "http://api.openweathermap.org/data/2.5/weather?zip=${zipCode},ru&appid=${apiKey}";
+
+  final response = await http.get(Url.parse(requestUrl));
+  if (response.statusCode == 200){
+    return WeatherInfo.fromJson(jsonDecode(response.body));
+  }else{
+    throw Exeption("Eroor loading request URL info.")
+  }
+}
+
+class WeatherInfo {
+  final String location;
+  final String weather;
+  final double temp;
+  final double tempMin;
+  final double tempMax;
+  final double windSpeed;
+  final int humidity;
+
+  WeatherInfo({
+    @required this.location,
+    @required this.temp,
+    @required this.tempMax,
+    @required this.tempMin,
+    @required this.weather,
+    @required this.windSpeed,
+    @required this.humidity
+})
+  factory WeatherInfo.formJson(Map<String.dynamic> json){
+    return WeatherInfo(
+        location: json['name'],
+        temp: json['main']['temp'],
+        tempMax: json['main']['temp_max'],
+        tempMin: json['main']['temp_min'],
+        weather: json['weaser'][0]['description'],
+        windSpeed: json['wind']['speed'],
+        humidity: json['main']['humidity']
+    );
+  }
+}
+
 
 void main () => runApp(
   MaterialApp(
@@ -15,61 +63,28 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyApp extends State<MyApp>{
+
+  late Future<WeatherInfo> futureWeather;
+
+    @override
+  void initSate(){
+    super.initState();
+    futureWeaser = fetcWeather();
+  }
+
   @override
   Widget build(BuildContext context) => Scaffold(
-      body: Column(
-        children: [
-          Container(
-            height: MediaQuery.of(context).size.height /2,
-            width: MediaQuery.of(context).size.width,
-            color: Color(0xfff1f1f1),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: const [
-                Text(
-                  "Location",
-                  style: TextStyle(
-                    fontSize: 30.0,
-                    fontWeight: FontWeight.w900
-                ),
-                ),
-                Padding(
-                  padding: EdgeInsets.only(top: 10.0, bottom: 10.0),
-                  child: Text(
-                    "35\u2103",
-                    style: TextStyle(
-                      color: Colors.purple,
-                      fontSize: 40.0,
-                      fontWeight: FontWeight.w900
-                    ),
-                  ),
-                ),
-                Text(
-                  "High of 38\u2103, Low of 30\u2103",
-                  style: TextStyle(
-                    color: Color(0xff9e9e9e),
-                    fontSize: 14.0,
-                    fontWeight: FontWeight.w600,
-                  ),
-                )
-              ],
-            ),
-          ),
-          Expanded(
-            child: Padding(
-              padding: EdgeInsets.all(20.0),
-                child: ListView(
-                  children: [
-                    WeatherTile(icon: Icons.thermostat_outlined, title: "Temperature", subtitle: "35\u2103"),
-                    WeatherTile(icon: Icons.filter_drama_outlined, title: "Weather", subtitle: "Cloudy"),
-                    WeatherTile(icon: Icons.wb_sunny, title: "Humidity", subtitle: "67%"),
-                    WeatherTile(icon: Icons.waves_outlined, title: "Wind speed", subtitle: "2 m/sec"),
-                  ],
-                )
-              ),
-            ),
+      body:FutureBuilder<WeatherInfo>(
+        future: futureWeather,
+        builder: (context, snapshot){
+          if(snapshot.hasData){
 
-        ],
-      ),
+      }else if(snapshot.hasError){
+        return Center(
+          child: Text("${snapshot.hasError}"),
+        );
+      }
+      }
+      )
     );
 }
